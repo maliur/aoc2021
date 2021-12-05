6b49eb6ae0d8b081ac3565a044801174d4a6b14e
Maliur <maliur@protonmail.com>
2021-12-05 15:35:13 +0100

    Day 1 part b

--- day-1.ts
+++ day-1.ts
@@ -5,7 +5,7 @@ const data: string = getInputFromFile(1);
 // part a
 const timesIncreased: number = data
   .split("\n")
-  .map(value => parseInt(value, 10))
+  .map((value) => parseInt(value, 10))
   .reduce(
     (
       previousValue: number,
@@ -14,9 +14,7 @@ const timesIncreased: number = data
       array: number[]
     ) => {
       const next = currentIndex + 1;
-      // console.log(next, array.length)
-      if (next < array.length) {
-      console.log(`${array[next]}  > ${currentValue}`)
+      if (next <= array.length) {
         if (array[next] > currentValue) {
           previousValue++;
         }
@@ -27,10 +25,39 @@ const timesIncreased: number = data
     0
   );
 
-console.log('Day 1 part A: ', timesIncreased);
+console.log("Day 1 part A: ", timesIncreased);
 
 // part b
+const timesSumIncreased = data
+  .split("\n")
+  .map((value) => parseInt(value, 10))
+  .reduce(
+    (
+      previousValue: number,
+      currentValue: number,
+      currentIndex: number,
+      array: number[]
+    ) => {
+      const window = 3;
+      const currentWindow = currentIndex + window;
+      const nextWindow = currentIndex + 1 + window;
 
-const timesSumIncreased = 0;
+      if (currentWindow <= array.length && nextWindow <= array.length) {
+        const sumCurrentWindow = array
+          .slice(currentIndex, currentWindow)
+          .reduce((m, v) => m + v);
+        const sumNextWindow = array
+          .slice(currentIndex + 1, nextWindow)
+          .reduce((m, v) => m + v);
 
-console.log('Day 1 part B: ', timesSumIncreased);
+        if (sumNextWindow > sumCurrentWindow) {
+          previousValue++;
+        }
+      }
+
+      return previousValue;
+    },
+    0
+  );
+
+console.log("Day 1 part B: ", timesSumIncreased);
